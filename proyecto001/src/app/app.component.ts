c2f672594a29f49a5c992046161a1aebdd099262
Cristobal Rosa <crisrogar@gmail.com>
2024-09-24 18:33:30 +0200

    update proyecto01 ejercicio angular 17

--- proyecto001/src/app/app.component.ts
+++ proyecto001/src/app/app.component.ts
@@ -10,5 +10,24 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'proyecto001';
+  nombre: string = 'Rodriguez Pablo';
+  edad: number = 40;
+  email: string = 'rpablo@gmail.com';
+  sueldos: number[] = [1700, 1600, 1900];
+  activo: boolean = true;
+
+  esActivo() :string {
+    if (this.activo)
+      return 'Trabajador activo';
+    else
+      return 'Trabajador inactivo';
+  }
+
+  ultimos3Sueldos() :number {
+    let suma: number = 0;
+    for (let index = 0; index < this.sueldos.length; index++)
+      suma += this.sueldos[index];
+
+    return suma;
+  }
 }
