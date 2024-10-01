6a24a0a3dd9084655f876446b14fbf699b7c92fd
Cristobal Rosa <crisrogar@gmail.com>
2024-10-01 12:51:28 +0200

    proyecto005 angular 17

---
+++ proyecto005/src/app/selectornumerico/selectornumerico.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selectornumerico',
+  standalone: true,
+  imports: [],
+  templateUrl: './selectornumerico.component.html',
+  styleUrl: './selectornumerico.component.css'
+})
+export class SelectornumericoComponent implements OnInit{
+  @Input() minimo :number = 1;
+  @Input() maximo :number = 1;
+  actual :number = 1;
+
+  ngOnInit(): void {
+    this.actual = this.minimo;
+  }
+
+  incrementar() :void {
+    if (this.actual < this.maximo) {
+      this.actual++;
+    }
+  }
+
+  decrementar() :void {
+    if (this.actual > this.minimo) {
+      this.actual--;
+    }
+  }
+
+  fijar(v :number) :void{
+    if (v >= this.minimo && v <= this.maximo) {
+      this.actual = v;
+    }
+  }
+}
